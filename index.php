7733c6ee1a3928559cb91b8ffad0e3088b9536a6
fullstack-devco <fullstacksoftwaredevco@gmail.com>
2019-08-28 17:11:32 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>HIEMS</title>
+    <link rel="stylesheet" href="css/bootstrap/bootstrap.css" />
+  </head>
+  <body>
+    <div>
+      <h3>
+        HIEMS
+      </h3>
+      <form action="" method="post">
+        <label for="email">Email</label>
+        <input type="email" name="email" placeHolder="Enter your email here">
+        <label for="password">Password</label>
+        <input type="password" name="password" placeHolder="Enter your password here">
+      </form>
+    </div>
+
+    <script src="js/bootstrap/bootstrap.js"></script>
+  </body>
+</html>
